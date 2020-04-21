ea7b8b1a5ac0f6263172b943233541a9852b3aca
Dana204 <44040637+Dana204@users.noreply.github.com>
2020-04-21 16:44:17 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret_key"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://Project1:password@localhost/Project1" # for local machine use
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://foawmaqxxdiizl:c04d6c9147e4b5429cb4d0c5f01a9e8999b447a6e724f8bba7ef54a02fded82a@ec2-52-87-58-157.compute-1.amazonaws.com:5432/d6dvr900ccamgj"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://oeijnwzcwluxmu:f82857995279fc41b70869b10e3abad8edcc43aacdb41fd9c6b91c709d394508@ec2-54-147-209-121.compute-1.amazonaws.com:5432/d7seuhs2uh13ob"
 # added just to suppress a warning
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
